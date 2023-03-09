825526e5e9764764b4dc010d9561c6818801f598
unknown <mndias29@gmail.com>
2023-03-09 11:57:54 -0300

    header ta basicamente pronta agr (esqueci de subir antes

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+// Chama os components para os arquivos html principais
+$(function(){
+    $('#header').load('../components/header.html');
+});
+
+// Inicia biblioteca de animações
+AOS.init({
+    offset: 200,
+    once: true,
+    duration: 600,
+});
+
+// Abre e fecha o menu mobile
+function menu(){
+    document.getElementById('menu').classList.toggle('active')
+}
